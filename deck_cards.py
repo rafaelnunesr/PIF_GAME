19cf23a50495229e56f73735f6d973dd37787db4
rafaelrios <rafaelrafssk@hotmail.com>
2019-03-23 13:30:09 -0300

    deck cards

---
+++ deck_cards.py
@@ -0,0 +1,27 @@
+from random import randint
+
+suits = 'HEARTS SPARES CLUBS DIAMONDS'.split()
+
+ranks = ['A', '2', '3', '4', '5', '6',
+         '7', '8', '9', '10', 'K', 'Q', 'J']
+
+french_cards = [(rank, suit) for suit in suits
+                             for rank in ranks
+                        ]
+
+for _ in range(2):
+    french_cards.append(('0', 'JOKER'))
+
+
+french_cards_shuffled = []
+
+
+while len(french_cards) > 0:
+
+    select = randint(0, len(french_cards)-1)
+
+    french_cards_shuffled.append(french_cards[select])
+
+    french_cards.pop(select)
+
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from deck_cards import french_cards_shuffled as deck
+
+print(deck)
