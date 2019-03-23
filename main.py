be3687461c0d35ed3ed9b6815dd8b0fdb22997b4
rafaelrios <rafaelrafssk@hotmail.com>
2019-03-23 14:23:45 -0300

    deck cards with color

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from deck_cards import french_cards_shuffled as deck
 
-print(deck)
+player = []
+cpu = []
+
+for i in range(18):
+    if i % 2 == 0:
+        player.append(deck.pop(i))
+    else:
+        cpu.append(deck.pop(i))
+
+
+colors = {'HEARTS':'\033[31m', 'SPARES':'\033[32m',
+          'CLUBS':'\033[34m', 'DIAMONDS':'\033[33m', 'JOKER':'\033[37m'}
+
+
+def sort_by_hierarchy():
+
+    suits = 'HEARTS SPARES CLUBS DIAMONDS JOKER'
+
+
+def show_player_cards():
+    for card in player:
+        rank = card[0]
+        suit = card[1]
+
+        print('{} {} {} {}'.format(colors[suit], rank, suit, '\033[0m'))
+
+show_player_cards()
