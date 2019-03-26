b5144327ab38ea18445a145177199a3ff086d35e
rafaelnunesr <rafaelrafssk@hotmail.com>
2019-03-26 13:15:02 -0300

    ...

--- interface.py
+++ interface.py
@@ -4,30 +4,29 @@ from tkinter import filedialog
 import os
 
 
-from player import player_cards
+from player import *
 
 
-
-file = os.getcwd() + '\cards\\'
+file = os.getcwd() + '\cards\\' # Windows
+# file = os.getcwd() + '/cards/' # MAC
 
 root = Tk()
 root.geometry("1000x700+300+150")
 root.resizable(width=True, height=True)
 root.title('PIF GAME')
 
-def open_img(file_image, position_x, position_y, size_x=180,
+
+def open_img(card_image, position_x, position_y, size_x=180,
              size_y=250, angulo=0):
 
-    img = Image.open(file_image)
+    img = Image.open(card_image)
     img = img.resize((size_x,size_y), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img.rotate(angulo))
     panel = Label(root, image=img)
     panel.image = img
     panel.place(x=position_x, y=position_y)
 
-def show_cards_tkinter():
-    name_card = 'BACK_CARD.png'
-    open_img((file + name_card), 350, 100)
+def show_player_cards_tkinter():
 
     position_x = 0
     x = 0
@@ -38,8 +37,23 @@ def show_cards_tkinter():
         position_x += 90
         x += 1
 
+def show_deck_cards():
+    img_deck_card = 'BACK_CARD.png'
+    open_img((file + img_deck_card), 350, 100)
 
-show_cards_tkinter()
+
+def ask_deck_card():
+
+    next_card = deck[0]
+    del deck[0]
+    next_card = next_card[0] + '_' + next_card[1] + '.png'
+    img_deck_card = next_card
+    open_img((file + img_deck_card), 350, 100)
+
+
+show_deck_cards()
+show_player_cards_tkinter()
+
 
 root.mainloop()
 
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from deck_cards import french_cards_shuffled as deck
 player_cards = []
 cpu_cards = []
 
-for i in range(18):
-    if i % 2 == 0:
-        player_cards.append(deck.pop(i))
-    else:
-        cpu_cards.append(deck.pop(i))
+def give_initial_cards():
+    for i in range(18):
+        if i % 2 == 0:
+            player_cards.append(deck.pop(i))
+        else:
+            cpu_cards.append(deck.pop(i))
+
+give_initial_cards()
+
