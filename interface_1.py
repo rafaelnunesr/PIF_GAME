208c2ce77aae0350821d314c1afe6ca754096e2b
rafaelrios <rafaelrafssk@hotmail.com>
2019-04-08 00:20:06 -0300

    code clean

--- interface_1.py
+++ interface_1.py
@@ -20,8 +20,11 @@ class Interface:
     player_fifth_card_up = False
     player_sixth_card_up = False
     player_seventh_card_up = False
-    player_eight_card_up = False
+    player_eighth_card_up = False
     player_nineth_card_up = False
+
+    player_cards_position_y = 380
+    player_cards_position_up = 300
 
     def __init__(self, master=None):
         self.master = master
@@ -30,15 +33,22 @@ class Interface:
         self.master.title('PIF GAME')
 
         self.cards_deck = self.deck_module()
-        self.player_first_card = self.first_card(0, 380)
-        self.player_second_card = self.second_card(0, 380)
-        self.player_third_card = self.third_card()
-        self.player_fourth_card = self.fourth_card()
-        self.player_fifth_card = self.fifth_card()
-        self.player_sixth_card = self.sixth_card()
-        self.player_seventh_card = self.seventh_card()
-        self.player_eighth_card = self.eighth_card()
-        self.player_nineth_card = self.nineth_card()
+        self.__card_game_positions()
+
+
+    def __card_game_positions(self, first=380, second=380, third=380,
+                              fourth=380, fifth=380, sixth=380, seventh=380,
+                              eighth=380, nineth=380):
+
+        self.player_first_card = self.first_card(0, first)
+        self.player_second_card = self.second_card(90, second)
+        self.player_third_card = self.third_card(180, third)
+        self.player_fourth_card = self.fourth_card(270, fourth)
+        self.player_fifth_card = self.fifth_card(360, fifth)
+        self.player_sixth_card = self.sixth_card(450, sixth)
+        self.player_seventh_card = self.seventh_card(540, seventh)
+        self.player_eighth_card = self.eighth_card(630, eighth)
+        self.player_nineth_card = self.nineth_card(720, nineth)
 
 
     def show_image(self, file_image, position_x, position_y, size_x=180,
@@ -90,14 +100,18 @@ class Interface:
         first_panel.bind('<Button-1>', self.first_card_clicked)
         first_panel.place(x=position_x, y=position_y)
 
+
     def first_card_clicked(self, event=0):
         global first_panel
         first_panel.destroy()
+
+        position_x = 0
+
         if self.player_first_card_up == False:
-            self.player_first_card = self.first_card(0, 300)
+            self.__card_game_positions(first=self.player_cards_position_up)
             self.player_first_card_up = True
         else:
-            self.player_first_card = self.first_card(0, 380)
+            self.__card_game_positions()
             self.player_first_card_up = False
 
 
@@ -105,8 +119,6 @@ class Interface:
         global second_panel
         card_name = player_cards[1][0] + '_' + player_cards[1][1] + '.png'
         file_image = file + card_name
-
-        position_x = 90; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         second_panel = Button(self.master, image=img)
@@ -117,18 +129,21 @@ class Interface:
     def second_card_clicked(self, event=0):
         global second_panel
         second_panel.destroy()
+
+        position_x = 90
+
         if self.player_second_card_up == False:
-            self.player_second_card = self.second_card(90, 300)
+            self.__card_game_positions(second=self.player_cards_position_up)
             self.player_second_card_up = True
         else:
-            self.player_second_card = self.second_card(90, 380)
+            self.__card_game_positions()
             self.player_second_card_up = False
 
-    def third_card(self):
+    def third_card(self, position_x, position_y):
+        global third_panel
+
         card_name = player_cards[2][0] + '_' + player_cards[2][1] + '.png'
         file_image = file + card_name
-
-        position_x = 180; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         third_panel = Button(self.master, image=img)
@@ -137,13 +152,22 @@ class Interface:
         third_panel.place(x=position_x, y=position_y)
 
     def third_card_clicked(self, event=0):
-        print('03')
+        global third_panel
+        third_panel.destroy()
 
-    def fourth_card(self):
+        position_x = 180
+
+        if self.player_third_card_up == False:
+            self.__card_game_positions(third=self.player_cards_position_up)
+            self.player_third_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_third_card_up = False
+
+    def fourth_card(self, position_x, position_y):
+        global fourth_panel
         card_name = player_cards[3][0] + '_' + player_cards[3][1] + '.png'
         file_image = file + card_name
-
-        position_x = 270; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         fourth_panel = Button(self.master, image=img)
@@ -152,13 +176,23 @@ class Interface:
         fourth_panel.place(x=position_x, y=position_y)
 
     def fourth_card_clicked(self, event=0):
-        print('04')
 
-    def fifth_card(self):
+        global fourth_panel
+        fourth_panel.destroy()
+
+        position_x = 270
+
+        if self.player_fourth_card_up == False:
+            self.__card_game_positions(fourth=self.player_cards_position_up)
+            self.player_fourth_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_fourth_card_up = False
+
+    def fifth_card(self, position_x, position_y):
+        global fifth_panel
         card_name = player_cards[4][0] + '_' + player_cards[4][1] + '.png'
         file_image = file + card_name
-
-        position_x = 360; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         fifth_panel = Button(self.master, image=img)
@@ -167,13 +201,22 @@ class Interface:
         fifth_panel.place(x=position_x, y=position_y)
 
     def fifth_card_clicked(self, event=0):
-        print('05')
+        global fifth_panel
+        fifth_panel.destroy()
 
-    def sixth_card(self):
+        position_x = 360
+
+        if self.player_fifth_card_up == False:
+            self.__card_game_positions(fifth=self.player_cards_position_up)
+            self.player_fifth_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_fifth_card_up = False
+
+    def sixth_card(self, position_x, position_y):
+        global sixth_panel
         card_name = player_cards[5][0] + '_' + player_cards[5][1] + '.png'
         file_image = file + card_name
-
-        position_x = 450; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         sixth_panel = Button(self.master, image=img)
@@ -182,13 +225,23 @@ class Interface:
         sixth_panel.place(x=position_x, y=position_y)
 
     def sixth_card_clicked(self, event=0):
-        print('06')
+        global sixth_panel
+        sixth_panel.destroy()
 
-    def seventh_card(self):
+        position_x = 450
+
+        if self.player_sixth_card_up == False:
+            self.__card_game_positions(sixth=self.player_cards_position_up)
+            self.player_sixth_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_sixth_card_up = False
+
+    def seventh_card(self, position_x, position_y):
+        global seventh_panel
         card_name = player_cards[6][0] + '_' + player_cards[6][1] + '.png'
         file_image = file + card_name
 
-        position_x = 540; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         seventh_panel = Button(self.master, image=img)
@@ -197,13 +250,22 @@ class Interface:
         seventh_panel.place(x=position_x, y=position_y)
 
     def seventh_card_clicked(self, event=0):
-        print('07')
+        global seventh_panel
+        seventh_panel.destroy()
 
-    def eighth_card(self):
+        position_x = 540
+
+        if self.player_seventh_card_up == False:
+            self.__card_game_positions(seventh=self.player_cards_position_up)
+            self.player_seventh_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_seventh_card_up = False
+
+    def eighth_card(self, position_x, position_y):
+        global eighth_panel
         card_name = player_cards[7][0] + '_' + player_cards[7][1] + '.png'
         file_image = file + card_name
-
-        position_x = 630; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         eighth_panel = Button(self.master, image=img)
@@ -212,13 +274,22 @@ class Interface:
         eighth_panel.place(x=position_x, y=position_y)
 
     def eighth_card_clicked(self, event=0):
-        print('08')
+        global eighth_panel
+        eighth_panel.destroy()
 
-    def nineth_card(self):
+        position_x = 630
+
+        if self.player_eighth_card_up == False:
+            self.__card_game_positions(eighth=self.player_cards_position_up)
+            self.player_eighth_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_eighth_card_up = False
+
+    def nineth_card(self, position_x, position_y):
+        global nineth_panel
         card_name = player_cards[8][0] + '_' + player_cards[8][1] + '.png'
         file_image = file + card_name
-
-        position_x = 720; position_y = 380
 
         img = self.show_image(file_image, position_x, position_y)
         nineth_panel = Button(self.master, image=img)
@@ -227,14 +298,17 @@ class Interface:
         nineth_panel.place(x=position_x, y=position_y)
 
     def nineth_card_clicked(self, event=0):
-        print('09')
+        global nineth_panel
+        nineth_panel.destroy()
 
-    def player_card_clicked(self, number):
-        print(number)
+        position_x = 720
 
-
-
-
+        if self.player_nineth_card_up == False:
+            self.__card_game_positions(nineth=self.player_cards_position_up)
+            self.player_nineth_card_up = True
+        else:
+            self.__card_game_positions()
+            self.player_nineth_card_up = False
 
 
 root = Tk()
