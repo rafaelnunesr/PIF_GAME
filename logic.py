f354718ecb46fca334c3d01c01dea5b6078f14f3
rafaelrios <dsi@rm2012>
2019-05-01 15:18:18 -0300

    clean

--- logic.py
+++ logic.py
@@ -13,39 +13,9 @@ class Logic:
 
     def __init__(self, list_cards):
         self.list_cards = list_cards
-        self.dict_list_cards = {}
-        self.sequence_A = []
-        self.sequence_B = []
-        self.sequence_C = []
-        self.extra_cards = []
 
-    def convert_rank_cards(self):
 
-        temp_list_cards = []
 
-        for card in self.list_cards:
-            rank, suits = card
-            if rank in self.cards_values:
-                rank = self.cards_values[rank]
-
-            temp_list_cards.append((int(rank), suits))
-
-        self.list_cards = (temp_list_cards)
-
-    def categorize_by_suit(self):
-
-        dict_cards = {
-            'CLUBS': [], 'DIAMONDS': [],
-            'HEARTS': [], 'SPADES': [],
-            'JOKER': []}
-
-        for card in self.list_cards:
-            dict_cards[card[1]].append(card[0])
-
-        self.dict_list_cards = dict_cards
-
-    def show_a(self):
-        print(self.dict_list_cards)
 
 
 
@@ -53,6 +23,4 @@ class Logic:
 
 
 run_logic = Logic(player_cards)
-run_logic.convert_rank_cards()
-run_logic.categorize_by_suit()
-# run_logic.show_a()
+
